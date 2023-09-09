f2a301781b2519c7ce914c9b4d1b9f1b2dd877b4
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-09-09 22:19:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ const container = document.querySelector(".particle-container");
 
 function createParticle() {
   const particle = document.createElement("div");
+  particle.classList.add("particle");
